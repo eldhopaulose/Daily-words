c88bd1246e16649320dfb0ea2c6fe1fe0c03e921
Eldho Paulose <eldhopaulose0485@gmail.com>
2023-03-25 23:59:16 +0530

    initial commit

---
+++ main.py
@@ -0,0 +1,24 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+
+import random
+import pandas as pd
+
+nba = pd.read_csv("Words.csv")
+
+
+
+@app.get("/")    
+async def root():
+    # Generate 100 random numbers between 1 and 100
+    random_numbers = random.sample(range(0, len(nba)), 5)
+    #create a list of dictionary
+    word_list = []
+    for i in random_numbers:
+
+        row_dict  = nba.loc[[i]].to_dict(orient='records')[0]
+        word_list.append(row_dict )
+        print(row_dict )
+    return{"words": word_list}
