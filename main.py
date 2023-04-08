cb6fe05ed3388f367172f9f6bcfa78a64801564e
Eldho Paulose <eldhopaulose0485@gmail.com>
2023-04-08 23:39:16 +0530

    cross add

--- main.py
+++ main.py
@@ -1,8 +1,28 @@
 import random
 import pandas as pd
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
 
 app = FastAPI()
+
+origins = [
+    "http://localhost.tiangolo.com",
+    "https://localhost.tiangolo.com",
+    "http://localhost",
+    "http://localhost:8080",
+    "http://localhost:5173",
+]
+
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 
 # Load csv file
 nba = pd.read_csv("DIct.csv")
