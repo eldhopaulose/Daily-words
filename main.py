ad5c8889d5244143eac1bf5eedee1bcf2e589a11
Eldho Paulose <eldhopaulose0485@gmail.com>
2023-04-08 20:27:56 +0530

    dictionary completed

--- main.py
+++ main.py
@@ -5,19 +5,45 @@ from fastapi import FastAPI
 app = FastAPI()
 
 # Load csv file
-nba = pd.read_csv("Words.csv")
+nba = pd.read_csv("DIct.csv")
 
-
-
-@app.get("/")    
+@app.get("/")
 async def root():
-    # Generate 100 random numbers between 1 and 100
+    # Generate 5 random numbers between 0 and len(nba)
     random_numbers = random.sample(range(0, len(nba)), 5)
-    #create a list of dictionary
+    # Create a list of dictionaries containing the randomly selected rows
     word_list = []
     for i in random_numbers:
+        row_dict = nba.loc[[i]].to_dict(orient='records')[0]
+        word_list.append(row_dict)
+    return {"words": word_list}
 
-        row_dict  = nba.loc[[i]].to_dict(orient='records')[0]
-        word_list.append(row_dict )
-        print(row_dict )
-    return{"words": word_list}
+@app.post("/word")
+async def get_word_meaning(input_word: str):
+    # Load dictionary DataFrame
+    columns = ['english_word', 'malayalam_definition']
+    df = pd.read_csv("DIct.csv", header=None, names=columns, skiprows=1)
+
+    # Convert input word to lowercase
+    input_word = input_word.strip().lower()
+
+    # Find all rows that contain the input word or sub-word
+    result = df.loc[df['english_word'].str.lower().str.contains(input_word)]
+
+    # If rows are found, return the meanings
+    if not result.empty:
+        # Filter rows that contain the exact input word
+        exact_matches = result[result['english_word'].str.lower() == input_word]
+        if not exact_matches.empty:
+            meanings = exact_matches['malayalam_definition'].tolist()
+            return {"word": input_word.capitalize(), "meaning": meanings}
+
+        # Filter rows that contain the input word as a sub-word
+        subword_matches = result[result['english_word'].str.lower() != input_word]
+        if not subword_matches.empty:
+            word_list = subword_matches['english_word'].tolist()[:5]
+            meaning_list = subword_matches['malayalam_definition'].tolist()[:5]
+            word_meaning_list = [{"word": word, "meaning": meaning} for word, meaning in zip(word_list, meaning_list)]
+            return {"words": word_meaning_list}
+    else:
+        return {"message": "Word not found in dictionary"}
